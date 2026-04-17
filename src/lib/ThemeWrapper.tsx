50e6fe48b9f48df91a085c8630bdf5c9ae731b3e
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-17 10:16:45 -0300

    feat: install tailwind css and shadc/ui

---
+++ src/lib/ThemeWrapper.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "next-themes";
+
+export function ThemeWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeWrapper } from "./lib/ThemeWrapper";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeWrapper>
+      <App />
+    </ThemeWrapper>
   </React.StrictMode>,
 );
