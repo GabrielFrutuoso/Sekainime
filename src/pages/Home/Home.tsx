7a5ef0abfe4d0133c5a539207684a2340521888a
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-17 18:23:10 -0300

    feat(WIP): home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
+import { AnimeCard } from "@/components/ui/AnimeCard/AnimeCard";
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 export function Home() {
-  const { category } = useParams<{ category?: string}>();
+  const { category } = useParams<{ category?: string }>();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -11,11 +12,19 @@ export function Home() {
     }
   }, [category, navigate]);
 
-  const currentCategory = category ?? "top-animes";
-
   return (
-    <div>
-      <h1>Category: {currentCategory}</h1>
+    <div className="p-2">
+      <div
+        className="p-4 bg-sidebar rounded-md grid grid-cols-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4"
+      >
+        {Array.from({ length: 24 }).map((_, index) => (
+          <AnimeCard
+            key={index}
+            name="Chainsaw Man"
+            poster="https://animefire.io/img/animes/chainsaw-man-large.webp"
+          />
+        ))}
+      </div>
     </div>
   );
 }
