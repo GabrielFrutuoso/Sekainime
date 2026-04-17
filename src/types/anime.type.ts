3c754244d95ab6edfd2f40cb080f18354519a3aa
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-17 18:22:45 -0300

    type: add types

---
+++ src/types/anime.type.ts
@@ -0,0 +1,22 @@
+import { Episode } from "./episode.type";
+import { Pagination } from "./pagination.type";
+
+
+export type Anime = {
+  name: string;
+  poster: string;
+};
+
+export type AnimeInfos = {
+  name: string;
+  japaneseName: string;
+  orientalName: string;
+  poster: string;
+  synopsis: string;
+  episodes: Episode[];
+};
+
+export type AnimePromise = {
+  animes: Anime[];
+  pagination: Pagination;
+};
