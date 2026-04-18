cb0625dfd568b46c92ac4478f4246be51b5f4493
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-17 23:58:27 -0300

    feat: add skeleton to anime card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { AnimeCard } from "@/components/ui/AnimeCard/AnimeCard";
+import { AnimeCardSkeleton } from "@/components/ui/AnimeCard/AnimeCardSkeleton";
 import { useApi } from "@/hooks/useApi";
 import { AnimePromise } from "@/types/anime.type";
 import { useEffect } from "react";
@@ -15,15 +16,19 @@ export function Home() {
   }, [category, navigate]);
 
   const endpoint = category ? `/animes/${category}` : "/animes/top-animes";
-  const { data } = useApi<AnimePromise>([endpoint], endpoint);
+  const { data, isLoading } = useApi<AnimePromise>([endpoint], endpoint);
   console.log(data);
 
   return (
     <div className="p-2">
       <div className="p-4 bg-sidebar rounded-md grid grid-cols-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-4">
-        {data?.animes?.map((anime, index) => (
-          <AnimeCard key={index} name={anime.name} poster={anime.poster} />
-        ))}
+        {isLoading
+          ? Array.from({ length: 24 }).map((_, index) => (
+              <AnimeCardSkeleton key={index} />
+            ))
+          : data?.animes?.map((anime, index) => (
+              <AnimeCard key={index} name={anime.name} poster={anime.poster} />
+            ))}
       </div>
     </div>
   );
