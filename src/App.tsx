c39dcad0c8103fafab72dd26feee3b3659557f05
GabrielFrutuoso <mackbiel03@gmail.com>
2026-04-26 18:46:38 -0300

    feat: add Titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,21 @@ import {
   ResizablePanelGroup,
 } from "./components/ui/resizable";
 import { BrowserRouter } from "react-router-dom";
+import { TitleBar } from "./components/ui/TitleBar/TitleBar";
 
 function App() {
   return (
-    <main className="h-screen overflow-hidden flex">
+    <main className="h-screen overflow-hidden flex flex-col">
+      <TitleBar />
       <QueryProvider>
         <BrowserRouter>
-          <ResizablePanelGroup className="h-full w-full">
-            <ResizablePanel defaultSize={300} minSize={250} maxSize={350}>
+          <ResizablePanelGroup className="flex-1 w-full">
+            <ResizablePanel defaultSize={250} minSize={250} maxSize={350}>
               <Sidebar />
             </ResizablePanel>
             <ResizableHandle />
             <ResizablePanel defaultSize={80}>
-              <div className="flex h-full">
+              <div className="flex h-full overflow-hidden bg-sidebar">
                 <AppRoutes />
               </div>
             </ResizablePanel>
